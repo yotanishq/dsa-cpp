f10bf7d5f74c57bac50321e75971e8e1caf1e65e
Tanishq Adhikari <252206544+yotanishq@users.noreply.github.com>
2025-12-31 18:27:24 +0530

    gcd(euclidean algorithm)

---
+++ maths/gcd.cpp
@@ -0,0 +1,23 @@
+// Euclidean Algorithm
+// Time Complexity: O(log min(a, b))
+// Used to compute GCD efficiently
+# include<iostream>
+using namespace std;
+int gcd ( int a, int b);
+int main(){
+    int a,b;
+    cout<<"Enter any two numbers : ";
+    cin>>a>>b; 
+    cout<<"The gcd of the numbers is : "<<gcd(a,b)<<endl;
+}
+int gcd ( int a, int b){
+    while (a!=0 && b!=0){
+        if(a>b) a=a%b;
+        else if (a<b) b=b%a;
+        else if (a==b) return a;
+
+    }
+    if (a==0) return b;
+    if (b==0) return a;
+    return 0;
+}
