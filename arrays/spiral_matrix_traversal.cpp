0c3a5e389dc2b6b11deae9185322ed718fe169ae
Tanishq Adhikari <252206544+yotanishq@users.noreply.github.com>
2026-01-16 13:33:28 +0530

    Spiral traversal of a matrix using shrinking boundaries

---
+++ arrays/spiral_matrix_traversal.cpp
@@ -0,0 +1,30 @@
+# include<iostream>
+# include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>>matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+    int row=matrix.size();
+    int column=matrix[0].size();
+    int top=0;
+    int right=column-1;
+    int bottom=row-1;
+    int left=0;
+    while (left<=right&&top<=bottom){
+        for (int j=left;j<=right;j++){
+            cout<<matrix[top][j]<<"\t";
+        }
+        top++;
+        for (int i=top;i<=bottom;i++){
+            cout<<matrix[i][right]<<"\t";
+        }
+        right--;
+        for (int j=right;j>=left;j--){
+            cout<<matrix[bottom][j]<<"\t";
+        }
+        bottom--;
+        for (int i=bottom;i>=top;i--){
+            cout<<matrix[i][left]<<"\t";
+        }
+        left++;
+    }
+}
